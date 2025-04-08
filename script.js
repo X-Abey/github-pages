ff8a418dd26a46368c64ff45acba676e691c3e41
X-Abey <am224953532@gmail.com>
2025-04-08 06:42:24 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// Add interactivity for future features like wallet connect, form submissions, etc.
+document.addEventListener('DOMContentLoaded', function () {
+    console.log("NovaCoin Website Loaded!");
+});
